b0ef13bacee2e5b453e0b060fa3bdeed3e2ea2ff
changle0245 <lc0245308@gmail.com>
2025-12-28 20:08:44 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['arabgoldfactory.com', 'www.arabgoldfactory.com'],
-    unoptimized: true
-  }
-}
-
-module.exports = nextConfig
